066fe555d3adcf1de3b138f584465a56eda7da1f
aymenben12 <124013771+aymenben12@users.noreply.github.com>
2026-09-10 04:30:26 +0100

    Add files via upload

---
+++ admin-service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "wasli-admin-cache-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(function() {
+        return caches.match(event.request);
+      })
+  );
+});
